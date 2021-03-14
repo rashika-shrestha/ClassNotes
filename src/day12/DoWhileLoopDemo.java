98bbf33f8a564cc677ca31345d9056f26a360d8a
rashika <rashika98@gmail.com>
2021-03-13 23:50:18 -0500

    File transfer

---
+++ src/day12/DoWhileLoopDemo.java
@@ -0,0 +1,13 @@
+package day12;
+
+public class DoWhileLoopDemo {
+
+    public static void main(String[] args) {
+        int a = 15;
+        do {
+            System.out.println(a);
+            a++;
+        } while (a < 8);
+        System.out.println("Exit from loop");
+    }
+}
---
+++ src/day13/NestedForLoopDemo.java
@@ -0,0 +1,14 @@
+package day13;
+
+public class NestedForLoopDemo {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.println(i + " " + j);
+            }
+            System.out.println();
+        }
+    }
+}
